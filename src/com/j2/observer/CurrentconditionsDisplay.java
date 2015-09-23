b59d06a891653dfa74539b396cc782f84be33eac
tmakwm <tmddjq4839@naver.com>
2015-09-23 13:45:46 +0900

    observer pattern

---
+++ src/com/j2/observer/CurrentconditionsDisplay.java
@@ -0,0 +1,20 @@
+package com.j2.observer;
+
+public class CurrentconditionsDisplay implements Observer{
+  private float temperature;
+  private float humidity;
+  private Subject weatherData;
+  public CurrentconditionsDisplay(Subject weatherData){
+    this.weatherData = weatherData;
+    weatherData.registerObserver(this);
+  }
+  public void update(float temperature, float humidity, float pressure){
+    this.temperature = temperature;
+    this.humidity=humidity;
+    display();
+  }
+  public void display(){
+    System.out.println("current conditions;"+temperature+humidity);
+  }
+}
+  
